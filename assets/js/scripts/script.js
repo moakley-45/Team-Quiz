b58d056a799c6e0ba912e84903380ae0ab80c51d
Louise Anderson <ddq4yosl@students.codeinstitute.net>
2024-10-23 13:40:07 +0000

    add js pseudo code

---
+++ assets/js/scripts/script.js
@@ -0,0 +1,26 @@
+/* Variables */
+
+
+/* Functions */
+
+
+/* Modal Functions */
+
+
+/** Shows the modal when needed */
+
+
+
+/** Hides the modal when needed */
+
+
+/** Loads the modal when page finishes loading */
+
+
+/** Adds an event listener - when the user submits
+ * a name, the modal is hidden and the Player Elements
+ * Section is updated with their data
+ */
+
+
+/* /Modal Functions */
